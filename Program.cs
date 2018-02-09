05995ea9107a308dca4c859fc7a08de359d1d6ca
David Ellison <David Ellison>
2018-02-09 16:51:05 -0500

    Second Commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static int factorial(int n)
+    {
+        // Complete this function
+        int total = 1;
+
+        if (n > 1)
+        {
+            total *= n * (n - 1);
+            total = n * factorial(n - 1);
+        }
+
+        return total;
+
+    }
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int result = factorial(n);
+        Console.WriteLine(result);
+    }
+}
